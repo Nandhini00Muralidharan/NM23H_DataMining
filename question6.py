8a2e68e85405ad1d5592d8e6161d97b003327cc5
Nandhini00Muralidharan <nandhini.00.murali@gmail.com>
2024-01-29 03:04:02 -0500

    Q6 Commit_2

--- question6.py
+++ question6.py
@@ -1,20 +1,20 @@
 def question6_1():
     answer = {
-        'equal_width': ['Change', 9],
-        'equal_frequency': ['Change', 9]
+        'equal_width': ['No change', 7],
+        'equal_frequency': ['No change', 7]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 9],
-        'equal_frequency': ['Change', 9]
+        'equal_width': ['No change', 8],
+        'equal_frequency': ['No change', 8]
     }
     return answer
 
 def question6_3():
     answer = {
-        'equal_width': ['Change', 4],
-        'equal_frequency': ['Change', 4]
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['Change', 2]
     }
     return answer
