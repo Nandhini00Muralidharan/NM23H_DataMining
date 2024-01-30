a189a58660ca9f9d4d91de7b674903d23b1561c6
Nandhini00Muralidharan <nandhini.00.murali@gmail.com>
2024-01-30 12:18:36 -0500

    Q3 Commit_2

--- question3.py
+++ question3.py
@@ -1,5 +1,5 @@
 def question3_1():
-    answer = ['discrete', 'quantitative', 'interval']
+    answer = ['continuous', 'quantitative', 'interval']
     return answer
 
 def question3_2():
@@ -7,7 +7,7 @@ def question3_2():
     return answer
 
 def question3_3():
-    answer = ['discrete', 'qualitative', 'ordinal']
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer 
 
 def question3_4():
